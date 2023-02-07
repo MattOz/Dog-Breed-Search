9b7ddf5e8802c414c0baec457c21ef1d85d2dfca
Matt Osborne <MattJOzzy@gmail.com>
2023-02-07 11:01:44 -0600

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+//Dog image search by breed
 var requestUrl = 'https://dog.ceo/api/breed/hound/images'
 
-fetch(requestUrl)
+fetch(requestUrl1)
     .then(function (response) {
         return response.json();
         })
         .then(function (data) {
         console.log(data)
     });
+
+//Dog facts
+var requestUrl2 = ''
