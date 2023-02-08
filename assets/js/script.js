db7156c5a833f345645f9fc533cbdbdd9b5d87d7
Matt Osborne <MattJOzzy@gmail.com>
2023-02-08 12:40:02 -0600

    created function to get dog fact, cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,8 @@
-//Dog image search by breed
-// var imageUrl = 'https://dog.ceo/api/breed/hound/images'
-// fetch(imageUrl)
-//     .then(function (response) {
-//         return response.json();
-//         })
-//         .then(function (data) {
-//         console.log(data)
-//     });
-
-//Dog facts
-// var factUrl = 'https://cors-anywhere.herokuapp.com/http://dog-api.kinduff.com/api/facts?number=1'
-// fetch(factUrl)
-//     .then(function (response) {
-//         return response.json();
-//         })
-//         .then(function (data) {
-//         console.log(data)
-//     });
-
 var searchButton = document.getElementById('searchBtn');
 searchButton.addEventListener('click', getImage);
+
+var factButton = document.getElementById('dogFactBtn');
+factButton.addEventListener('click', getFact);
 
 function getImage () {
     var userInput = document.getElementById('searchBar').value;
@@ -27,7 +10,6 @@ function getImage () {
     var requestUrl2 = requestUrl.replaceAll(' ', '-');
     var imageContainer = document.getElementById('image-container');
     console.log(requestUrl2);
-
     fetch(requestUrl2)
     .then(function (response) {
         return response.json();
@@ -38,5 +20,19 @@ function getImage () {
         var img = document.createElement('img');
         img.src = dogImageUrl;
         imageContainer.appendChild(img);
+    });
+}
+
+function getFact () {
+    var factContainer = document.getElementById("dogFactDisplay");
+    var factUrl = 'https://cors-anywhere.herokuapp.com/http://dog-api.kinduff.com/api/facts?number=1'
+    fetch(factUrl)
+    .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        var fact = data.facts[0];
+        console.log(fact);
+        factContainer.append(fact);
     });
 }
